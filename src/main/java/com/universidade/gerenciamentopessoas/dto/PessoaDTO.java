aec0003d39a3b9334195b91b792f93b5d72945b2
Playza27 <giulia.karolina.gk@gmail.com>
2025-04-11 14:06:54 -0300

    Commit 2.1

--- src/main/java/com/universidade/gerenciamentopessoas/dto/PessoaDTO.java
+++ src/main/java/com/universidade/gerenciamentopessoas/dto/PessoaDTO.java
@@ -11,4 +11,37 @@ public class PessoaDTO {
     private String nome;
     private String cpf;
     private Integer idade;
+
+    public String getNome() {
+    return nome;
+    }
+
+    public Integer getIdade() {
+    return idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public void setId(Long id) {
+    }
+
+    public void setNome(String nome) {
+    }
+
+    public void setIdade(Integer idade) {
+    }
+
+    public void setCpf(String cpf) {
+    }
+
+    public Long setId() {
+    return id;
+    }
 }
--- src/main/java/com/universidade/gerenciamentopessoas/model/Pessoa.java
+++ src/main/java/com/universidade/gerenciamentopessoas/model/Pessoa.java
@@ -22,4 +22,40 @@ public class Pessoa {
 
     @Column(nullable = false)
     private Integer idade;
+
+
+    public Pessoa(String nome, Integer idade, Long id, String cpf) {
+        this.nome = nome;
+        this.idade = idade;
+        this.id = id;
+        this.cpf = cpf;
+    }
+
+
+    public String getNome(){
+        return nome;
+    }
+     public void setNome(String nome){
+        this.nome = nome;
+     }
+
+    public Integer getIdade(){
+        return idade;
+    }
+    public void setIdade(Integer idade){
+        this.idade = idade;
+    }
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+    public String getCpf(){
+        return cpf;
+    }
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
 }
