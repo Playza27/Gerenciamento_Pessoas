434931c0311bb5ba39ac4e050a8eebff8f167935
Giulia <giulia.karolina.gk@gmail.com>
2025-04-13 21:39:47 -0300

    Update PessoaDTO.java

--- src/main/java/com/universidade/gerenciamentopessoas/dto/PessoaDTO.java
+++ src/main/java/com/universidade/gerenciamentopessoas/dto/PessoaDTO.java
@@ -12,36 +12,4 @@ public class PessoaDTO {
     private String cpf;
     private Integer idade;
 
-    public String getNome() {
-    return nome;
-    }
-
-    public Integer getIdade() {
-    return idade;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-
-    public void setId(Long id) {
-    }
-
-    public void setNome(String nome) {
-    }
-
-    public void setIdade(Integer idade) {
-    }
-
-    public void setCpf(String cpf) {
-    }
-
-    public Long setId() {
-    return id;
-    }
 }
